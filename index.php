e3b8798559c81143e594b557e7140d831cd6181b
Gilbert Steven Yabut <g.yabut@integratrcorp.com>
2023-03-21 11:24:37 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+
+<body>
+<?php
+$fruits = [
+    'apple',
+    'mango',
+    'pineapple'
+];
+
+$fruits2 = [
+    'apple',
+    'mango'
+];
+
+$fruits = count($fruits);
+
+// $fruits[] = 'guava';
+// $fruits[] = 'grapes';
+
+// $fruits = array_merge($fruits, [
+//     'grapes', 'lychee'
+// ]);
+
+// $fruits_label = implode("<br />", $fruits);
+
+// foreach ($fruits as $fruit) {
+//     $fruits_label .= $fruit . "<br/>";
+// }
+
+// $count = 10;
+// $print_count = "";
+// for ($i = 0; $i <= $count; $i++) {
+//     if ($i % 2) {
+//         $print_count .= $i . "<br />";
+//     }
+// }
+?>
+<h1><?php echo $fruits ?></h1>
+</body>
